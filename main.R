8b4d4dd053f190ea393065e725924da2471611b4
Kamil <kamil.uhc@op.pl>
2018-06-19 20:47:25 +0200

    Change model params, add shapiro-wilk test

--- main.R
+++ main.R
@@ -12,23 +12,29 @@ model_physical_1 <- lm(PPG ~ Height + MPG + Weight + Age, data = data)
 summary(model_physical_1)
 AIC(model_physical_1)
 logLik(model_physical_1) 
+shapiro.test(residuals(model_physical_1))
+
 
 model_physical_2 <- lm(PPG ~ MPG + Weight + Age, data = data)
 summary(model_physical_2)
-#plot(model_physical_2)
 AIC(model_physical_2)
 logLik(model_physical_2) 
+plot(model_physical_2)
+shapiro.test(residuals(model_physical_2))
 
 model_physical_3 <- lm(PPG ~ MPG + BMI + Age, data = data)
 summary(model_physical_3)
 AIC(model_physical_3)
 logLik(model_physical_3)
+plot(model_physical_3)
+shapiro.test(residuals(model_physical_3))
 
 ########################################################
 #2 dodatowe kategoryczne - Position
-model_position <- lm(PPG ~ Pos + FG_p + ThreeP_p + FT_p + FGA, data = data) #jedna wyrzuca i dodaje jako stałą
+model_position <- lm(PPG ~ Pos + MPG, data = data) #jedna wyrzuca i dodaje jako stałą
 summary(model_position)
 AIC(model_position)
 logLik(model_position)
+shapiro.test(residuals(model_position))
 
 #3 jeden z interakcjami - Birth_place  * tam gdzie interackja
