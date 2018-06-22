cecc8418ccf68d47658777cdb0ff05966e38e9b8
Misznik <40390779+Misznik@users.noreply.github.com>
2018-06-22 17:00:00 +0200

    Removed model constant

--- main.R
+++ main.R
@@ -1,5 +1,6 @@
 #setwd("C:/Users/micha/Documents/GitHub/ModelsOfLinearRegression")
 library(readxl)
+library(car)
 data <- read_excel("data.xlsx")
 
 data$PPG <- round(data$PTS / data$`Games Played`, 2)
@@ -10,28 +11,36 @@ data$MPG <- round(data$MIN / data$`Games Played`, 2)
 ########################################################################
 model_physical_1 <- lm(PPG ~ Height + MPG + Weight + Age, data = data)
 summary(model_physical_1)
+#plot(model_physical_1)
 AIC(model_physical_1)
 logLik(model_physical_1) 
 shapiro.test(residuals(model_physical_1))
 
+model_physical_4 <- lm(PPG ~ Height + MPG + Weight + Age - 1, data = data)
+summary(model_physical_4)
+AIC(model_physical_4)
+#plot(model_physical_4)
+logLik(model_physical_4) 
+shapiro.test(residuals(model_physical_4))
 
-model_physical_2 <- lm(PPG ~ MPG + Weight + Age, data = data)
+model_physical_2 <- lm(PPG ~ MPG + Weight + Age - 1, data = data)
 summary(model_physical_2)
 AIC(model_physical_2)
 logLik(model_physical_2) 
-plot(model_physical_2)
+#plot(model_physical_2)
 shapiro.test(residuals(model_physical_2))
 
-model_physical_3 <- lm(PPG ~ MPG + BMI + Age, data = data)
+model_physical_3 <- lm(PPG ~ MPG + BMI + Age - 1, data = data)
 summary(model_physical_3)
 AIC(model_physical_3)
 logLik(model_physical_3)
-plot(model_physical_3)
+#plot(model_physical_3)
 shapiro.test(residuals(model_physical_3))
+
 
 ########################################################
 #2 dodatowe kategoryczne - Position
-model_position <- lm(PPG ~ Pos + MPG, data = data) #jedna wyrzuca i dodaje jako stałą
+model_position <- lm(PPG ~ Pos + MPG - 1, data = data) #jedna wyrzuca i dodaje jako stałą
 summary(model_position)
 AIC(model_position)
 logLik(model_position)
@@ -39,12 +48,18 @@ shapiro.test(residuals(model_position))
 
 #3 jeden z interakcjami - Birth_place  * tam gdzie interackja
 # gracze z US grają więcej minut, wiecej zaufania od trenerów
-model_origin <- lm(PPG ~ MPG * Birth_Place, data = data) #jedna wyrzuca i dodaje jako stałą
-summary(model_origin)
+model_origin <- lm(PPG ~  MPG * Birth_Place - 1, data = data) #jedna wyrzuca i dodaje jako stałą
+summary(model_origin) #AGE JAKO INTERAKCJA?
 AIC(model_origin)
 logLik(model_origin)
+shapiro.test(residuals(model_origin))
 
-model_origin <- lm(PPG ~ MPG + Birth_Place, data = data) #jedna wyrzuca i dodaje jako stałą
-summary(model_origin)
-AIC(model_origin)
-logLik(model_origin)
+Anova(model_physical_1, type = 2)
+anova(model_physical_1, model_physical_2, model_physical_3, model_physical_4)
+mydata <- data.frame(data$PPG,  data$MPG, data$Height, data$Weight)
+cor(mydata)
+plot(model_physical_4)
+abline(h = c(-3,3), col = "blue")  #odstające
+abline(v = 4/length(residuals(model_physical_4)) * 3, col = "red") #wysoka dźwignia
+#t.test(residuals(model))
+#cor(data$PPG, data$PPG*data$MPG) tak sie interakcje znajduje?
