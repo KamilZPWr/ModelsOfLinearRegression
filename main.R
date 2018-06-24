2a85916f774a38b15c9f0eb1240fceeafbcb1adc
Misznik <40390779+Misznik@users.noreply.github.com>
2018-06-24 13:15:21 +0200

    Checked log model

--- main.R
+++ main.R
@@ -1,35 +1,70 @@
 setwd("C:/Users/micha/Documents/GitHub/ModelsOfLinearRegression")
 library(readxl)
 library(car)
-library(mclust)
 data <- read_excel("data.xlsx")
 data$PPG <- round(data$PTS / data$`Games Played`, 2)
 data$MPG <- round(data$MIN / data$`Games Played`, 2)
 data$FTAPG <- round(data$FTA / data$`Games Played`, 2)
 data$FGAPG <- round(data$FGA / data$`Games Played`, 2)
 data$ThreeAPG <- round(data$ThreePA / data$`Games Played`, 2)
-data$Pos <- as.factor(data$Pos)
+data$Pos <- as.factor(data$Pos)  #czy to konieczne?
 data$Birth_Place <- as.factor(data$Birth_Place)
+data <- data[ , -which(names(data) %in% c("OREB","DREB","REB","AST","STL","TOV","BLK","PF","EFF","AST/TOV","STL/TOV","DREB"))]
 
-N<-1000 #powtórzenia Monte Carlo
-checkConstMean <- function(res){# test stałej średniej residuów
+ConstMeanMC <- function(res){# test stałej średniej residuów
+  N <- 10000
   mean(
-    sapply(1:N, function(...){
-      t.test(sample(res,500))$p.value<0.05     
+    sapply(1:N, function(...){  #losowe probki z danych
+      t.test(sample(res, 50))$p.value < 0.05     
     })
   )
 }
-checkConstVar <- function(res){ # test stałej wariancji residuów
+ConstVarMC <- function(res){ # test stałej wariancji residuów
+  N <- 10000
   mean(
     sapply(1:N, function(...){
-      var.test(sample(res,500), sample(res,500))$p.value<0.05 
+      var.test(sample(res, 50), sample(res, 50))$p.value < 0.05 
     })
   )
 }
 
-#MODEL1 - Wpływ fizyczności i czasu gry na zdobywane punkty
-#1 same zmienne ciagle 
-########################################################################
+#MODEL1 same zmienne ciagle 
+model_offense <- lm(PPG ~ MPG + FGAPG + FGp + ThreeAPG + ThreePp + FTAPG + FTp - 1, 
+                    data = data)
+summary(model_offense)
+AIC(model_offense)
+logLik(model_offense) 
+shapiro.test(residuals(model_offense))
+plot(model_offense)
+
+ConstMeanMC(residuals(model_offense))
+ConstVarMC(residuals(model_offense))
+
+#MODEL 2 dodatowe kategoryczne - Position
+model_position <- lm(PPG ~ Pos + MPG + Age - 1, data = data) 
+summary(model_position)
+AIC(model_position)
+logLik(model_position)
+shapiro.test(residuals(model_position))
+plot(model_position)
+
+ConstMeanMC(residuals(model_position))
+ConstVarMC(residuals(model_position))
+
+
+#MODEL 3 jeden z interakcjami - Birth_place 
+model_origin <- lm(PPG ~  MPG * Birth_Place - 1, data = data)
+summary(model_origin)
+AIC(model_origin)
+logLik(model_origin)
+shapiro.test(residuals(model_origin))
+plot(model_origin)
+
+ConstMeanMC(residuals(model_origin))
+ConstVarMC(residuals(model_origin))
+
+###################################################################
+#Dodatkowe modele
 model_physical_1 <- lm(PPG ~ Height + MPG + Weight + Age, data = data)
 summary(model_physical_1)
 AIC(model_physical_1)
@@ -37,7 +72,7 @@ logLik(model_physical_1)
 shapiro.test(residuals(model_physical_1))
 plot(model_physical_1)
 
-model_physical_4 <- lm(PPG ~ Height + MPG + Weight + Age - 1, #to tworzy nowe odstające :x
+model_physical_4 <- lm(PPG ~ Height + MPG + Weight + Age - 1,
                        data = data)
 summary(model_physical_4)
 plot(model_physical_4)
@@ -50,43 +85,15 @@ model_physical_2 <- lm(PPG ~ MPG + Weight + Age - 1, data = data)
 summary(model_physical_2)
 AIC(model_physical_2)
 logLik(model_physical_2) 
-#plot(model_physical_2)
 shapiro.test(residuals(model_physical_2))
 
 model_physical_3 <- lm(PPG ~ MPG + BMI + Age - 1, data = data)
 summary(model_physical_3)
 AIC(model_physical_3)
 logLik(model_physical_3)
-#plot(model_physical_3)
 shapiro.test(residuals(model_physical_3))
 
-model_offense <- lm(PPG ~ FGAPG + FGp + ThreeAPG + ThreePp + FTAPG + FTp - 1, 
-                    data = data)
-summary(model_offense)
-AIC(model_offense)
-logLik(model_offense) 
-shapiro.test(residuals(model_offense))
-plot(model_offense)
-#temp1 <- c(which(abs(residuals(model11)/sd(residuals(model11))) >= 3))
-#temp2<- c(which(hatvalues(model11) > 6/length(residuals(model11)) * 3))
-#[-temp1,]
-
-
-########################################################
-#2 dodatowe kategoryczne - Position
-model_position <- lm(PPG ~ Pos + MPG + Age - 1, data = data) #jedna wyrzuca i dodaje jako stałą
-summary(model_position)
-AIC(model_position)
-logLik(model_position)
-shapiro.test(residuals(model_position))
-
-#3 jeden z interakcjami - Birth_place  * tam gdzie interackja
-# gracze z US grają więcej minut, wiecej zaufania od trenerów
-model_origin <- lm(PPG ~  MPG * Birth_Place - 1, data = data) #jedna wyrzuca i dodaje jako stałą
-summary(model_origin) #AGE JAKO INTERAKCJA?
-AIC(model_origin)
-logLik(model_origin)
-shapiro.test(residuals(model_origin))
+################################################################################
 
 Anova(model_physical_1, type = 2)
 anova(model_physical_1, model_physical_2, model_physical_3, model_physical_4)
@@ -94,45 +101,17 @@ mydata <- data.frame(data$PPG,  data$MPG, data$Height, data$Weight)
 cor(mydata)
 plot(model_physical_4)
 abline(h = c(-3,3), col = "blue")  #odstające
-abline(v = 4/length(residuals(model_physical_4)) * 3, col = "red") #wysoka dźwignia
-which(abs(residuals(model_physical_4)/sd(residuals(model_physical_4))) > 3)
-#Które są odstające dla modelu 4
+abline(v = 2/length(residuals(model_offense)) * 3, col = "black") #wysoka dźwignia
+which(abs((residuals(model_offense) - mean(residuals(model_offense)))/
+            sd(residuals(model_offense))) > 3)
 
-#t.test(residuals(model))
-#cor(data$PPG, data$PPG*data$MPG) tak sie interakcje znajduje?
-
-temp<-c()
-dowywalenia <- c()
-i<-0
-dane <- data.frame(data)
-while (i==0){
-model_physical_4 <- lm(PPG ~ Height + MPG + Weight + Age - 1, #to tworzy nowe odstające :x
-                       data = dane) 
-if (shapiro.test(residuals(model_physical_4))$`p.value` > 0.05)  {
-  print('SW')
-  break
-  }
-temp <- c(which(abs(residuals(model_physical_4)/sd(residuals(model_physical_4))) > 3))
-if ( length(temp) == 0) {
-  print('Brak odstajacych') 
-  break
-}
-dowywalenia <- c(dowywalenia, temp)
-dane <- dane[-temp,]
-}
-shapiro.test(residuals(model_physical_4))
-View(dane)
-
-temp2 <- c(which(hatvalues(model_physical_4) > 4/length(residuals(model_physical_4)) * 3))
-dane <- dane[-temp2,]
-
-###############3
-
+###########################################
+#Odrzucanie wartości odstających i o wysokiej dźwigni
 dane <- data.frame(data)
 temp1 <- c()
 temp2 <- c()
 while (TRUE){
-  model11 <- lm(PPG ~ FGAPG + FG_p + ThreeAPG + ThreeP_p + FTAPG + FT_p - 1, 
+  model11 <- lm(PPG ~ MPG + FGAPG + FGp + ThreeAPG + ThreePp + FTAPG + FTp - 1, 
                 data = dane)
   shapiro.test(residuals(model11))
   if (shapiro.test(residuals(model11))$`p.value` > 0.05) {
@@ -153,20 +132,25 @@ while (TRUE){
   }else{
     dane <- dane[-temp2,]
   }
-  temp1 <- c(which(abs(residuals(model11)/sd(residuals(model11))) > 3))
-  temp2 <- c(which(hatvalues(model11) > 6/length(residuals(model11)) * 3))
+  temp1 <- c(which(abs((residuals(model_offense) - mean(residuals(model_offense)))/sd(residuals(model11))) > 3))
+  temp2 <- c(which(hatvalues(model11) > 7/length(residuals(model11)) * 3))
   if(length(temp2)==0 & length(temp1)==0){
     break
   }
 }
-###########################
-model_offense <- lm(PPG ~ FGAPG + FGp + ThreeAPG + ThreePp + FTAPG + FTp - 1, 
-              data = data)
+
+############################### LOGARYTM
+
+model_offense <- lm(PPG ~ MPG + FGAPG + FGp + ThreeAPG + ThreePp + FTAPG + FTp - 1, 
+                    data = dane)
 summary(model_offense)
 AIC(model_offense)
 logLik(model_offense) 
 shapiro.test(residuals(model_offense))
 plot(model_offense)
-#temp1 <- c(which(abs(residuals(model11)/sd(residuals(model11))) >= 3))
-#temp2<- c(which(hatvalues(model11) > 6/length(residuals(model11)) * 3))
-#[-temp1,]
+
+dane <- data
+dane$PPG <- log(dane$PPG)
+temp1 <- c(which(abs((residuals(model_offense) - mean(residuals(model_offense)))/sd(residuals(model11))) > 3))
+temp2 <- c(which(hatvalues(model11) > 7/length(residuals(model11)) * 3))
+dane <- dane[-temp2,]
